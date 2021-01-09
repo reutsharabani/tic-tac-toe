76b0e606b6a47ebb3fa06a4e011100ff16b4556a
Reut Sharabani <reut.sharabani@gmail.com>
2021-01-09 16:42:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Basic tic-tac-toe with re-agent. Probably not best-practice since I waas just playing around.
+
+Do not take as an example.
---
+++ src/xoxo/core.cljs
@@ -0,0 +1,105 @@
+(ns xoxo.core
+  (:require [reagent.core :as reagent :refer [atom]]
+            [reagent.dom :as rd]))
+
+(enable-console-print!)
+
+(println "This text is printed from src/xoxo/core.cljs. Go ahead and edit it and see reloading in action.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:count          1
+                          :current-player "X"}))
+
+(defn inc-handler [_]
+  (swap! app-state update :count inc))
+
+(defn taken-by [x y]
+  (get-in @app-state [:board x y]))
+
+(defn check-victory []
+  (let [rows    (for [x (range 3)]
+                  (into []
+                        (for [y (range 3)]
+                          [x y])))
+        columns (for [y (range 3)]
+                  (into []
+                        (for [x (range 3)]
+                          [x y])))
+        cross-1 [[[0 0] [1 1] [2 2]]]
+        cross-2 [[[0 2] [1 1] [2 0]]]]
+    (doseq [streak (concat rows columns cross-1 cross-2)]
+      (js/console.log (str "testing streak" streak))
+      (let [occupiers (frequencies (map #(apply taken-by %) streak))]
+        (println occupiers)
+        (when (contains? #{{"X" 3} {"Y" 3}} occupiers)
+          (swap! app-state assoc :winning-streak (set streak))
+          (swap! app-state assoc :winner (-> occupiers keys first)))))))
+
+(defn place [x y]
+  (fn []
+    (swap! app-state assoc-in [:board x y] (:current-player @app-state))
+    (check-victory)
+    (swap! app-state update :current-player (fn [cp]
+                                              (if (= cp "X")
+                                                "Y"
+                                                "X")))
+    ))
+
+(defn winner? [s]
+  (:winner s))
+
+(defn cell [x y]
+  [:td {:class         (str "board-cell"
+                            (when (contains? (:winning-streak @app-state) [x y])
+                                             (js/console.log (str "winner cell" x y))
+                                           " winner-cell"))
+        :border        "10px"
+        :key           (str "cell " x y)
+        :on-click      (when-not (or (taken-by x y)
+                                     (winner? @app-state))
+                         (place x y))
+        :on-mouse-out #(swap! app-state assoc :hovered-cell [])
+        :on-mouse-over #(when-not (or (taken-by x y)
+                                     (winner? @app-state))
+                          (swap! app-state assoc :hovered-cell [x y]))}
+   (let [alt-piece (when (= [x y] (:hovered-cell @app-state))
+                     (:current-player @app-state))]
+     (or (taken-by x y) alt-piece))])
+
+(defn row [x]
+  [:tr {:class "board-row"
+        :key   (str "row-" x)}
+   (doall (for [y (range 3)]
+            (cell x y)))])
+
+(defn winner []
+  [:tr [:td
+        {:colSpan "3"
+         :class   "winner"} (str (winner? @app-state) " won")]])
+
+(defn board []
+  [:table
+   [:tbody
+    [:tr
+     [:th {:on-click #(reset! app-state {:current-player "X"})
+           :class    "reset-button"
+           :colSpan  "3"} "reset"]]
+    (doall
+      (for [x (range 3)]
+        (row x)))
+    (if (winner? @app-state)
+      (winner))]])
+
+(defn xoxo []
+  [:div
+   (board)])
+
+(rd/render [xoxo]
+           (. js/document (getElementById "app")))
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+  )
